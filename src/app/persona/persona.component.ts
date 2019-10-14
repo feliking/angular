50767da95bcd2651962407410964236e5057dcb6
felix <felixddxd@gmail.com>
2019-10-13 22:21:41 -0400

    lección 16

---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent {
+
+  nombrePersona:string = 'Juan';
+  apellidoPersona:string = 'Perez';
+  edad:number = 28
+
+  
+
+}
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrls: ['./personas.component.css']
+
+})
+
+export class PersonasComponent{
+    agregarPersona = false;
+    agregarPersonaStatus = "No se ha agregado ninguna persona";
+    tituloPersona = "";
+
+    constructor(){
+        setTimeout(
+            () => {
+                this.agregarPersona = true
+            }
+            ,3000)
+    }
+    onCrearPersona(){
+        this.agregarPersonaStatus = "Persona Agregada"
+    }
+    onModificarPersona(event: Event){
+        this.tituloPersona = (<HTMLInputElement>event.target).value;
+    }
+}
